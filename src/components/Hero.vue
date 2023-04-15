e5f0b1b319614234eb4f4b16439e4dec1b1143bf
Anuoluwapo Abolarin <abolarin.aanu@gmail.com>
2023-04-15 22:51:04 +0100

    Project Setup

---
+++ src/components/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Testing One Two</h1>
+  <h3>Testing One Two</h3>
+  <p class="body-1">Testing One Two</p>
+  <p class="body-2">Testing One Two</p>
+  <p class="body-3">Testing One Two</p>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header />
+  <Hero />
+  <NewArrivals />
+  <Collections />
+  <BestSellers />
+  <LookBook />
+  <Campaign />
+  <About />
+  <Footer />
+</template>
+
+<script setup>
+import Header from "../components/Header.vue";
+import Hero from "../components/Hero.vue";
+import NewArrivals from "../components/NewArrivals.vue";
+import Collections from "../components/Collections.vue";
+import BestSellers from "../components/BestSellers.vue";
+import LookBook from "../components/LookBook.vue";
+import Campaign from "../components/Campaign.vue";
+import About from "../components/About.vue";
+import Footer from "../components/Footer.vue";
+</script>
