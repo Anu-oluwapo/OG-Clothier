e9fb6257670c7f78f0b92e8b8ae8ebc161c3f590
Anuoluwapo Abolarin <abolarin.aanu@gmail.com>
2023-04-17 00:08:15 +0100

    Worked on header and hero

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,20 @@
-<template></template>
+<template>
+  <header>
+    <div class="container">
+      <div class="nav-links">
+        <div class="nav-link"><a>HOME</a></div>
+        <div class="nav-link"><a>SHOP</a></div>
+        <div class="nav-link"><a>LOOKBOOK</a></div>
+        <div class="logo">
+          <a><img src="../assets/logo1.svg" /></a>
+        </div>
+        <div class="nav-link"><a>ABOUT</a></div>
+        <div class="nav-link"><a>SEARCH</a></div>
+        <div class="nav-link"><a>CART</a></div>
+      </div>
+    </div>
+  </header>
+</template>
 
 <script setup></script>
 
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,25 @@
 <template>
-  <h1>Testing One Two</h1>
+  <!-- <h1>Testing One Two</h1>
   <h3>Testing One Two</h3>
   <p class="body-1">Testing One Two</p>
   <p class="body-2">Testing One Two</p>
-  <p class="body-3">Testing One Two</p>
+  <p class="body-3">Testing One Two</p> -->
+
+  <section class="hero-section">
+    <div class="container">
+      <div class="reel-container">
+        <h1>OG CLOTHIER</h1>
+        <div class="play-reel">
+          <p class="body-1">Play <br />Reel</p>
+        </div>
+        <CircularArrow />
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import CircularArrow from "../components/icons/CircularArrow.vue";
+</script>
 
 <style lang="scss" scoped></style>
